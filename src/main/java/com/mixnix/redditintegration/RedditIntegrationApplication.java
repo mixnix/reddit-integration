6ebb9041eddf9fed27da85c9d52e0dadee69afc3
Michał Nielipiński <bukaniex@gmail.com>
2020-12-22 10:36:29 +0100

    Initial commit

---
+++ src/main/java/com/mixnix/redditintegration/RedditIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.mixnix.redditintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mixnix/redditintegration/RedditIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixnix.redditintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
