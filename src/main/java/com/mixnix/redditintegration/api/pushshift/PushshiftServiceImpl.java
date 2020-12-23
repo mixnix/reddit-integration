0f7f2c0416a0c22e776a78a43a5825a26a8989a1
Michał Nielipiński <bukaniex@gmail.com>
2020-12-23 14:52:16 +0100

    URL won't change without code changes so it shouldn't go to application properties

--- src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftServiceImpl.java
+++ src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftServiceImpl.java
@@ -2,7 +2,6 @@ package com.mixnix.redditintegration.api.pushshift;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,12 +10,10 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class PushshiftServiceImpl implements PushshiftService{
 
-    @Value("${resource.submissions}/?size=100&q={query}")
-    private String subredditSubmissionWithQuery;
-
     private final RestTemplate restTemplate;
 
     public RedditDataDTO findByQuery(String queryString) {
+        String subredditSubmissionWithQuery = "https://api.pushshift.io/reddit/search/submission";
         return restTemplate.getForObject(subredditSubmissionWithQuery, RedditDataDTO.class,
                 queryString);
     }
