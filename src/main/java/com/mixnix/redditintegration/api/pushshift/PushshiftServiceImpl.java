d10b63515239d939f40ab796777e6dc15fa73a2b
Michał Nielipiński <bukaniex@gmail.com>
2021-01-04 09:13:41 +0100

    uses query parameter again

--- src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftServiceImpl.java
+++ src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftServiceImpl.java
@@ -1,24 +1,25 @@
 package com.mixnix.redditintegration.api.pushshift;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class PushshiftServiceImpl implements PushshiftService{
 
     private final RestTemplate restTemplate;
 
     public RedditResponseDTO findByQuery(String queryString) {
-        String subredditSubmissionWithQuery = "https://api.pushshift.io/reddit/search/submission";
+        // query string can be anything because it searches by title and post content
+        String subredditSubmissionWithQuery = "https://api.pushshift.io/reddit/search/submission/?size=100" +
+                "&q={queryString}";
 
         RedditDataDTO redditDataDTO = restTemplate.getForObject(subredditSubmissionWithQuery, RedditDataDTO.class,
                 queryString);
-        return new RedditResponseDTO(redditDataDTO.getData().stream().map(RedditSubmissionDTO::getUrl).collect(Collectors.toList()));
+        return new RedditResponseDTO(redditDataDTO.getData().stream().map(RedditSubmissionDTO::getUrl)
+                .collect(Collectors.toList()));
     }
 }
--- src/main/java/com/mixnix/redditintegration/api/pushshift/RedditResponseDTO.java
+++ src/main/java/com/mixnix/redditintegration/api/pushshift/RedditResponseDTO.java
@@ -1,11 +1,11 @@
 package com.mixnix.redditintegration.api.pushshift;
 
-import lombok.AllArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Getter
 public class RedditResponseDTO {
-    //maybe there should be 2 DTO objects
     List<String> urls;
 }
