48afb5dcfe7fe1be10901349814368e1eaaf4c33
Michał Nielipiński <bukaniex@gmail.com>
2021-01-12 08:46:50 +0100

    Split converter into two functions for easier unit testing

--- src/main/java/com/mixnix/redditintegration/api/pushshift/RedditDataDTOConverter.java
+++ src/main/java/com/mixnix/redditintegration/api/pushshift/RedditDataDTOConverter.java
@@ -7,7 +7,11 @@ import java.util.stream.Collectors;
 @Service
 public class RedditDataDTOConverter {
     public RedditResponseDTO convertToRedditResponseDTO(RedditDataDTO redditDataDTO){
-        return new RedditResponseDTO(redditDataDTO.getData().stream().map(RedditSubmissionDTO::getUrl)
+        return new RedditResponseDTO(redditDataDTO.getData().stream().map(this::convertToString)
                 .collect(Collectors.toList()));
+    }
+
+    String convertToString(RedditSubmissionDTO redditSubmissionDTO){
+        return redditSubmissionDTO.getUrl();
     }
 }
