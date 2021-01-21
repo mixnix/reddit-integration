0e39f7fc449cb5859e69ba7975570315ca7a9f85
Michał Nielipiński <bukaniex@gmail.com>
2021-01-21 19:24:40 +0100

    method for downloading posts from given reddit

--- src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftService.java
+++ src/main/java/com/mixnix/redditintegration/api/pushshift/PushshiftService.java
@@ -2,4 +2,5 @@ package com.mixnix.redditintegration.api.pushshift;
 
 public interface PushshiftService {
     RedditResponseDTO findByQuery(String queryString, int pageSize);
+    RedditResponseDTO downloadFromReddit(String subreddit, int pageSize);
 }
